b5e94dd0e233830e3df84031b838efc4b6d3cc08
Wasith Theerapattrathamrong <phai@live.com>
2019-03-08 13:37:32 +0700

    Init

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -1,20 +1,5 @@
-//
-//  ViewController.swift
-//  FaceDetection
-//
-//  Created by Wasith Theerapattrathamrong on 8/3/2562 BE.
-//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
 }
 
